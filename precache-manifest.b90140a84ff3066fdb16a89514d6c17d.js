a054fa41b3c46cf819009e17ac49638cf890a391
Tosin <seanamos98@gmail.com>
2020-09-25 01:25:00 +0100

    Updates

---
+++ precache-manifest.b90140a84ff3066fdb16a89514d6c17d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fcb11ada83452bfac1428527a61466",
+    "url": "/budget-calculator/index.html"
+  },
+  {
+    "revision": "1e8b894e664bdffe0cb1",
+    "url": "/budget-calculator/static/css/main.63effb3f.chunk.css"
+  },
+  {
+    "revision": "636c6d9a21d69414100d",
+    "url": "/budget-calculator/static/js/2.71ec5f61.chunk.js"
+  },
+  {
+    "revision": "1e8b894e664bdffe0cb1",
+    "url": "/budget-calculator/static/js/main.796da1a1.chunk.js"
+  },
+  {
+    "revision": "e13d29f9126db5c1cdd8",
+    "url": "/budget-calculator/static/js/runtime-main.55b36272.js"
+  }
+]);
